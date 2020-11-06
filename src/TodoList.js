f85e8920797aa1103d5be3cca6d8fdb349333445
Ories Zhu <tellt@126.com>
2020-11-06 16:46:31 +0800

    完成tolist基本样式

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
+import 'antd/dist/antd.css'
+import {Button, Input, List} from 'antd'
+
+const data = ['早8点开晨会1','早8点开晨会2','早8点开晨会3']
 
 class TodoList extends Component {
   render() {
     return (
-      <div>
-        Hello world
+      <div style={{margin:'10px'}}>
+        <div>
+          <Input placeholder='Write Something' style={{ width:'250px', marginRight: '10px' }}/>
+          <Button type='primary'>增加</Button>
+          <div style={{margin:'10px',width:'300px'}}>
+            <List border
+                  dataSource={data}
+                  renderItem={item=>(<List.Item>{item}</List.Item>)}
+            />
+
+          </div>
+        </div>
       </div>
     );
   }
