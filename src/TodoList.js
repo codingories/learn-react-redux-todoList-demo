6ffe66035975ce0e8f562d8f5cf1cc660359c3d8
Ories Zhu <tellt@126.com>
2020-11-29 20:25:21 +0800

    将action type的type抽离到公共的actionTypes.js

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import 'antd/dist/antd.css'
 import {Button, Input, List} from 'antd'
 import store from './store'
+import { CHANGE_INPUT, ADD_ITEM, DELETE_ITEM } from './store/actionTypes'
 
 class TodoList extends Component {
 
@@ -37,26 +38,26 @@ class TodoList extends Component {
   }
   changeInputValue = (e) => {
     const action = {
-      type: 'changeInput',
+      type: CHANGE_INPUT,
       value: e.target.value
     } // 建立action
     store.dispatch(action)
   }
-  storeChange = ()=>{
-    this.setState(store.getState())
-  }
   clickBtn = ()=>{
     const action = {
-      type: 'addItem'
+      type: ADD_ITEM
     }
     store.dispatch(action)
   }
   deleteItem = (index)=>{
     const action = {
-      type: 'deleteItem',
+      type: DELETE_ITEM,
       index
     }
     store.dispatch(action) // 传递到store, reducer
+  }
+  storeChange = ()=>{
+    this.setState(store.getState())
   }
 }
 
