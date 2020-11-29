e545963e1858775b6e19cedd93893af2885087f4
Ories Zhu <tellt@126.com>
2020-11-29 13:18:05 +0800

    input输入打印出输入值

--- src/TodoList.js
+++ src/TodoList.js
@@ -8,13 +8,18 @@ class TodoList extends Component {
   constructor(props){
     super(props)
     this.state = store.getState()
+    // this.changeInputValue = this.changeInputValue.bind(this)
   }
 
   render() {
     return (
       <div style={{margin:'10px'}}>
         <div>
-          <Input placeholder={this.state.inputValue} style={{ width:'250px', marginRight: '10px' }}/>
+          <Input
+            placeholder={this.state.inputValue}
+            style={{ width:'250px', marginRight: '10px' }}
+            onChange={(e)=>this.changeInputValue(e)}
+          />
           <Button type='primary'>增加</Button>
           <div style={{margin:'10px',width:'300px'}}>
             <List bordered
@@ -25,6 +30,9 @@ class TodoList extends Component {
         </div>
       </div>
     );
+  }
+  changeInputValue(e){
+    console.log(e.target.value)
   }
 }
 
